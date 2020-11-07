41af26d3d556c5287a27c9879e454099538aa331
MekImEne <fi_mekideche@esi.dz>
2020-11-07 14:35:05 +0100

    init project

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React , {Component} from 'react'
+
+export default class Navbar extends Component {
+    render(){
+        return (
+            <div>
+                <h3>Hi, this is the navbar</h3>
+            </div>
+        );
+    }
+}
