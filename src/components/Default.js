9c5d8c24f7c80ff116ccff33186b6de4186ec76a
MekImEne <fi_mekideche@esi.dz>
2020-11-09 01:55:54 +0100

    fix all features in Cart components

--- src/components/Default.js
+++ src/components/Default.js
@@ -4,8 +4,7 @@ export default class Default extends Component {
     render() {
         return (
             <div>
-                <h3>Page not found</h3>
-                
+                <h3>Page not found</h3>               
             </div>
         )
     }
